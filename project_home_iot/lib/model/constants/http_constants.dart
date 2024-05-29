1c8947393a7f909a75528785331762a8ebf9c839
PhamTrung012224 <trung.pham01152224@gmail.com>
2024-05-29 13:43:31 +0700

    hide aio key

--- project_home_iot/lib/model/constants/http_constants.dart
+++ project_home_iot/lib/model/constants/http_constants.dart
@@ -1,5 +1,5 @@
 final class HttpConstants {
-  static const xAioKey = 'aio_tRQR248ABOV60zycmKfVzxd39u1N';
+  static const xAioKey = '';
   static const adafruitBaseUrl = 'http://io.adafruit.com/api/v2';
   static const weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 }
